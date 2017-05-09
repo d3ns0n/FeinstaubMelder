872c4484f72faca2b32035ae1af149b7374d2ad0
Dennis Lotz <d3ns0n@d3ns0n.de>
2017-05-09 15:15:02 +0200

    extract parameters and store them in config.json, add requirements.txt
    This is a first small step to improve code readability. It is connected
    to #1

--- tweetbeialarm.py
+++ tweetbeialarm.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# depends: python-requests
 
 import json
 
 import requests
 import tweepy
+
+
+def load_config():
+    with open('config.json') as json_data_file:
+        return json.load(json_data_file)
 
 
 # Auswerte Funktion
@@ -24,45 +28,41 @@ def pick_values(sensor):
         if a:
             # in der Regel ist der erste Wert der PM10
             result = (parsed_json[l - 1]['sensordatavalues'][0]['value'])
-            return (result)
+            return result
     # Falls Json unvollst  ndig ist
     return (0)
 
 
-# Freiburger Sensornummern, einfach neu dazugekommene hier an die Liste dranh  ngen
-sd = [533, 1224, 1288, 928, 1210, 1264, 1685, 1615, 1667]
+config = load_config()
+
+sensors = config["sensors"]
 maxlist = []
-for x in sd:
-    print(x)
-    url = 'http://api.luftdaten.info/static/v1/sensor/' + str(x) + '/'
+for sensor in sensors:
+    print(sensor)
+    sensor_url = 'http://api.luftdaten.info/static/v1/sensor/{}/'.format(sensor)
     # Liste erzeugen mit den Werten, ok float() ist nicht ganz sauber...
-    maxlist.append(float(pick_values(url)))
+    maxlist.append(float(pick_values(sensor_url)))
 
 # welches ist der h  chste Wert ?
 maxwert = max(maxlist)
 # zu welchem Sensor aus der Liste sd geh  rt der H  chstwert ?
-sensor = sd[maxlist.index(maxwert)]
+maxsensor = sensors[maxlist.index(maxwert)]
 
 tweet = ' Aktuell liegen keine Grenzwert- Überschreitungen in Freiburg vor '
 alarm = False
 
 # hier kannst du den Maxwert anpassen
-if maxwert > 5:
-    tweet = 'Achtung Freiburg! Feinstaubwerte hoch - Sensor: {} ist bei PM10 {} µg/m³'.format(sensor, maxwert)
+if maxwert > config["max_value"]:
+    tweet = 'Achtung Freiburg! Feinstaubwerte hoch - Sensor: {} ist bei PM10 {} µg/m³'.format(maxsensor, maxwert)
     # hier den Tweet ausl  sen
     alarm = True
 
 print tweet
 
-# acess tokens
-CONSUMER_KEY = 'x'
-CONSUMER_SECRET = 'x'
-ACCESS_KEY = 'x'
-ACCESS_SECRET = 'x'
-
+tokens = config["tokens"]
 # OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
+auth.set_access_token(tokens["access_key"], tokens["access_secret"])
 
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
