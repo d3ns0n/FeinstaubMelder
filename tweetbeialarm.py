c7ed74ffdfb07465096c7ddfe0aa28c0d1a845e9
Dennis Lotz <d3ns0n@d3ns0n.de>
2017-05-11 14:56:09 +0200

    refactor code into methods, use dictionary for sensor value pair

--- tweetbeialarm.py
+++ tweetbeialarm.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import decimal
 import json
+import operator
 
 import requests
 import tweepy
@@ -16,7 +18,7 @@ def perform_request(sensor):
     response = requests.get('http://api.luftdaten.info/static/v1/sensor/{0}/'.format(sensor))
     if response.status_code and not 200 <= response.status_code < 300:
         # Request konnte nicht erfolgreich ausgeführt werden
-        print('Luftdaten API error respone: status code = {0}'.format(response.status_code))
+        print('Luftdaten API error response: status code = {0}'.format(response.status_code))
         return 0
     return response
 
@@ -29,7 +31,7 @@ def parse_response(response):
         if a:
             # in der Regel ist der erste Wert der PM10
             result = (parsed_json[l - 1]['sensordatavalues'][0]['value'])
-            return result
+            return decimal.Decimal(result)
     # Falls Json unvollständig ist
     return 0
 
@@ -43,39 +45,33 @@ def get_pm10_value(sensor):
     return parse_response(response)
 
 
+def post_tweet(tweet_text):
+    tokens = config["tokens"]
+    # OAuth process, using the keys and tokens
+    auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
+    auth.set_access_token(tokens["access_key"], tokens["access_secret"])
+
+    # Creation of the actual interface, using authentication
+    api = tweepy.API(auth)
+
+    # twittern nur Text
+    api.update_status(status=tweet_text)
+
+
 config = load_config()
 
 sensors = config["sensors"]
-maxlist = []
+maxlist = {}
 for sensor in sensors:
-    print(sensor)
-    # Liste erzeugen mit den Werten, ok float() ist nicht ganz sauber...
-    maxlist.append(float(get_pm10_value(sensor)))
+    # Sensor und PM10 Wert als Key-Value Pair im Dictionary speichern
+    maxlist[sensor] = get_pm10_value(sensor)
 
-# welches ist der höchste Wert ?
-maxwert = max(maxlist)
-# zu welchem Sensor aus der Liste sd gehört der Höchstwert ?
-maxsensor = sensors[maxlist.index(maxwert)]
-
-tweet = ' Aktuell liegen keine Grenzwert- Überschreitungen in Freiburg vor '
-alarm = False
+# das Key-Value Pair mit dem höchsten Sensorwert ermitteln
+maxsensor = max(maxlist.iteritems(), key=operator.itemgetter(1))
 
 # hier kannst du den Maxwert anpassen
-if maxwert > config["max_value"]:
-    tweet = 'Achtung Freiburg! Feinstaubwerte hoch - Sensor: {0} ist bei PM10 {1} µg/m³'.format(maxsensor, maxwert)
+if maxsensor[1] > config["max_value"]:
+    tweet = 'Achtung Freiburg! Feinstaubwerte hoch - Sensor: {0} ist bei PM10 {1} µg/m³' \
+        .format(maxsensor[0], maxsensor[1])
     # hier den Tweet auslösen
-    alarm = True
-
-print tweet
-
-tokens = config["tokens"]
-# OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
-auth.set_access_token(tokens["access_key"], tokens["access_secret"])
-
-# Creation of the actual interface, using authentication
-api = tweepy.API(auth)
-
-# twittern nur Text
-if alarm:
-    api.update_status(status=tweet)
+    post_tweet(tweet)
