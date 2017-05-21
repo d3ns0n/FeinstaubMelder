3ca4c467c8092a7b0c6b0b6f7c34ea49d9df0e40
Dennis Lotz <d3ns0n@d3ns0n.de>
2017-05-21 12:44:19 +0200

    add test case to test load_config

--- test/test_tweetbeialarm.py
+++ test/test_tweetbeialarm.py
@@ -5,11 +5,23 @@ import mock
 import tweetbeialarm
 
 
-class TestPerformRequest(unittest.TestCase):
+class LoadConfigTest(unittest.TestCase):
+    @mock.patch('tweetbeialarm.json.load')
+    @mock.patch('tweetbeialarm.open')
+    def test_load_config_should_load_correct_file(self, open_function, load_function):
+        open_function.return_value = mock.MagicMock()
+
+        tweetbeialarm.load_config()
+
+        args, kwargs = open_function.call_args
+        assert args == ('config.json',)
+
+
+class PerformRequestTest(unittest.TestCase):
     sensor_id = 1337
 
     @mock.patch('tweetbeialarm.requests.get')
-    def test_perform_request_should_request_correct_url(self, get_function):
+    def test_perform_request_should_build_url_correct(self, get_function):
         get_function.return_value = mock.MagicMock(status_code=200)
 
         tweetbeialarm.perform_request(self.sensor_id)
